04e5034f3cf56b9d0bb299f088306649dbc45f59
MarioPaez <mariopaez_99@hotmail.com>
2025-05-13 13:55:10 +0200

    best example of go routines and channels

--- semana4-5/goRoutinesAndChannels/main.go
+++ semana4-5/goRoutinesAndChannels/main.go
@@ -2,27 +2,52 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
-func boring(msg string) <-chan string {
-
-	c := make(chan string)
-	go func() {
-		for i := 0; ; i++ {
-			c <- fmt.Sprintf("%d\n", i)
-			time.Sleep(100000)
-		}
-	}()
-	return c
+func productor(name string, out chan<- string) {
+	for i := range 5 {
+		time.Sleep(time.Duration(rand.Intn(1200)) * time.Millisecond)
+		out <- fmt.Sprintf("%s: mensaje %d", name, i)
+	}
+	close(out)
 }
 
 func main() {
-	c := boring("Boring!")
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	ch3 := make(chan string)
 
-	for range 5 {
-		fmt.Printf("You say: %s\n", <-c)
+	go productor("ch1", ch1)
+	go productor("ch2", ch2)
+	go productor("ch3", ch3)
+
+	for ch1 != nil || ch2 != nil || ch3 != nil {
+		select {
+		case res1, ok := <-ch1:
+			if !ok {
+				ch1 = nil
+			}
+			fmt.Println(res1)
+
+		case res2, ok := <-ch2:
+			if !ok {
+				ch2 = nil
+			}
+			fmt.Println(res2)
+		case res3, ok := <-ch3:
+			if !ok {
+				ch3 = nil
+			}
+			fmt.Println(res3)
+
+		case <-time.After(1 * time.Second):
+			fmt.Println("Nadie ha respondido en 1s")
+		}
+
 	}
 
-	fmt.Println("Me piro!")
+	fmt.Println("Todos los canales han terminado")
+
 }
