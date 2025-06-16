454c465eb33942e29b0e879384cd9ffe9ec8098d
MarioPaez <mariopaez_99@hotmail.com>
2025-06-16 23:42:07 +0200

    Trying go test -race and how we can solve it with channels

--- semana6-7/testing/concurrency/concurrency.go
+++ semana6-7/testing/concurrency/concurrency.go
@@ -2,13 +2,23 @@ package concurrency
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	url   string
+	valid bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
-
+	resultsChannel := make(chan result)
 	for _, url := range urls {
 		go func() {
-			results[url] = wc(url)
+			resultsChannel <- result{url, wc(url)}
 		}()
+	}
+
+	for range len(urls) {
+		r := <-resultsChannel
+		results[r.url] = r.valid
 	}
 
 	return results
