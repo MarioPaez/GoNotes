5f0a66e89466f80210bf02c8637f82255b35d9e3
MarioPaez <mariopaez_99@hotmail.com>
2025-06-19 19:52:38 +0200

    more reflection examples

--- semana6-7/testing/reflection/reflection.go
+++ semana6-7/testing/reflection/reflection.go
@@ -1,15 +1,30 @@
 package reflection
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 func walk(x any, fn func(input string)) {
-	val := reflect.ValueOf(x)
+	val := newFunction(x)
+
+	fmt.Println("valor de numField: ", val.NumField())
 	for i := range val.NumField() {
 		field := val.Field(i)
-		if field.Kind() == reflect.String {
+
+		switch field.Kind() {
+		case reflect.String:
 			fn(field.String())
+		case reflect.Struct:
+			walk(field.Interface(), fn)
 		}
-
 	}
+}
 
+func newFunction(x any) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	return val
 }
